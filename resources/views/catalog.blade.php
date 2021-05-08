a1845e08f34e2780d87c9c31fb115e4b767e287a
Ildar Meyker <ildar.meyker@gmail.com>
2021-05-08 18:05:40 +0300

    Add subscribe.

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -102,11 +102,11 @@
 
 	<section class="subscribe">
         <div class="container">
-            <h2 class="h3">Be part of our plant-life addicts community</h2>
+            <h2 class="h3 subscribe__h1">Be part of our plant-life addicts community</h2>
 
-            <form action="#">
-                <input type="text" placeholder="ENTER YOUR EMAIL HERE">
-                <button type="submit">ОК</button>
+            <form action="#" class="subscribe__form">
+                <input type="text" placeholder="ENTER YOUR EMAIL HERE" class="subscribe__input">
+                <button type="submit" class="subscribe__button">ОК</button>
             </form>
         </div>
 	</section>
